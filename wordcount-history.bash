56fbd5ea300debc6d953fc47701ba48411d59dca
Jeff Geerling <geerlingguy@mac.com>
2015-09-19 08:56:12 -0700

    Fix wordcount bash script.

--- wordcount-history.bash
+++ wordcount-history.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-# See: http://stackoverflow.com/a/9247215/100134
+# Count all lines in .txt files in a repository for each commit.
 for commit in `git rev-list --all`; do
     git log -n 1 --pretty=%ad --date=short $commit
     git archive $commit *.txt | tar -x -O | wc -w
