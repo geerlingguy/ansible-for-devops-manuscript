6079dd8b431be9cc3c82d42f1a201014e697dc96
Jeff Geerling <geerlingguy@mac.com>
2023-06-17 23:48:54 -0500

    Update wordcount history script to actually work with changing files.

--- wordcount-history.bash
+++ wordcount-history.bash
@@ -2,5 +2,6 @@
 # Count all lines in .txt files in a repository for each commit.
 for commit in `git rev-list --all`; do
     git log -n 1 --pretty=%ad --date=short $commit
-    git archive $commit *.txt | tar -x -O | wc -w
+    # On GNU tar, add `--wildcards --no-anchored` options
+    git archive $commit | tar -x -O '*.txt' | wc -w
 done
